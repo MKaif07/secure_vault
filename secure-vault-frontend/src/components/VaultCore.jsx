4186454b677ca6d0f739ebdfb1885eef8a1275b0
MK <kaif2002mk@gmail.com>
2026-04-27 15:27:24 +0530

    fixed expand issue

--- secure-vault-frontend/src/components/VaultCore.jsx
+++ secure-vault-frontend/src/components/VaultCore.jsx
@@ -121,7 +121,7 @@ export default function VaultCore() {
       </div>
 
       {/* FILE GRID */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-start">
         {isLoading ? (
           // Loading Skeletons
           [1, 2, 3].map((i) => (
